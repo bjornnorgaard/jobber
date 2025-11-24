62843b178e81c71d3d28400edec0a44961edd11e
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2025-11-24 17:41:04 +0100

    Integrated Hangfire with PostgreSQL in Jobber.WebApi and updated configuration. Added `compose.yml` for PostgreSQL service.

---
+++ src/Jobber.Hang/HangConfigurator.cs
@@ -0,0 +1,30 @@
+﻿using Hangfire;
+using Hangfire.PostgreSql;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace Jobber.Hang;
+
+public static class HangConfigurator
+{
+    public static void AddHang(WebApplicationBuilder builder)
+    {
+        builder.Services.AddHangfire(configuration =>
+        {
+            var cs = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UsePostgreSqlStorage(o => o.UseNpgsqlConnection(cs));
+        });
+
+        builder.Services.AddHangfireServer();
+    }
+
+    public static void UseHang(WebApplication app)
+    {
+        app.MapHangfireDashboard();
+    }
+}
--- src/Jobber.WebApi/Program.cs
+++ src/Jobber.WebApi/Program.cs
@@ -1,6 +1,11 @@
+using Jobber.Hang;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
+HangConfigurator.AddHang(builder);
 
 var app = builder.Build();
 app.MapOpenApi();
+HangConfigurator.UseHang(app);
+
 app.Run();
